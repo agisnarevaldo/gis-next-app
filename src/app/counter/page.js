dd26117066adfb4dc4ecc74fb39cfee167dbafa1
Agisna Revaldo <agisrifaldo75@gmail.com>
2023-11-16 20:58:17 +0700

    just commit

--- src/app/counter/page.js
+++ src/app/counter/page.js
@@ -11,12 +11,18 @@ export default function Page() {
   function decrement() {
     setCount(count -1);
   }
+
+  function reset() {
+    setCount(0);
+  }
   
   return(
     <div className="flex h-screen items-center justify-center">
       <button onClick={increment} className="px-2 bg-white text-black h-min">+</button>
       <p className="w-24 bg-blue-700 text-center h-min">{count}</p>
       <button onClick={decrement} className="px-2 bg-white text-black h-min">-</button>
+      <br />
+      <button onClick={reset} className="px-2 bg-red-700 h-min">C</button>
     </div>
   )
 }
