78bc662efc6c4569a28cf2a6a0054e61420653b2
Agisna Revaldo <agisrifaldo75@gmail.com>
2023-11-18 23:46:44 +0700

    add page: calculator

--- src/app/calculator/page.js
+++ src/app/calculator/page.js
@@ -1,36 +1,55 @@
+'use client'
+import { useState } from 'react'
+import './calculator.css'
+
 export default function page() {
+  let [result, setResult] = useState("0");
+
+  const handleClick = (e) => {
+    if (result.lengt >= 16) {
+      setResult("Inputan terlalu besar");
+      return;
+    }
+    if (result.charAt(0) === '0') {
+      result = result.slice(1, result.length);
+    }
+    setResult(result.concat(e.target.name));
+  }
+  
   return (
-    <div>
-      <div className="calc-screen"></div>
-      <div className="row">
-        <button>C</button>
-        <button>+/-</button>
-        <button>%</button>
-        <button>/</button>
+    <div className='container'>
+      <div className="calc-screen">
+        {result}
       </div>
       <div className="row">
-        <button>7</button>
-        <button>8</button>
-        <button>9</button>
-        <button>x</button>
+        <button name='clear' onClick={handleClick}>C</button>
+        <button name='+' onClick={handleClick}>+/-</button>
+        <button name='%' onClick={handleClick}>%</button>
+        <button name='/' onClick={handleClick}>/</button>
       </div>
       <div className="row">
-        <button>4</button>
-        <button>5</button>
-        <button>6</button>
-        <button>-</button>
+        <button name='7' onClick={handleClick}>7</button>
+        <button name='8' onClick={handleClick}>8</button>
+        <button name='9' onClick={handleClick}>9</button>
+        <button name='*' onClick={handleClick}>x</button>
       </div>
       <div className="row">
-        <button>1</button>
-        <button>2</button>
-        <button>3</button>
-        <button>+</button>
+        <button name='4' onClick={handleClick}>4</button>
+        <button name='5' onClick={handleClick}>5</button>
+        <button name='6' onClick={handleClick}>6</button>
+        <button name='-' onClick={handleClick}>-</button>
       </div>
       <div className="row">
-        <button>.</button>
-        <button>0</button>
-        <button>[x]</button>
-        <button>=</button>
+        <button name='1' onClick={handleClick}>1</button>
+        <button name='2' onClick={handleClick}>2</button>
+        <button name='3' onClick={handleClick}>3</button>
+        <button name='+' onClick={handleClick}>+</button>
+      </div>
+      <div className="row">
+        <button name='.' onClick={handleClick}>.</button>
+        <button name='0' onClick={handleClick}>0</button>
+        <button name='backspace' onClick={handleClick}>[x]</button>
+        <button name='equal' onClick={handleClick}>=</button>
       </div>
     </div>
   )
--- src/app/page.js
+++ src/app/page.js
@@ -50,9 +50,6 @@ export default function Home() {
               -&gt;
             </span>
           </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
         </a>
 
         <a
@@ -66,9 +63,19 @@ export default function Home() {
               -&gt;
             </span>
           </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
+        </a>
+
+        <a
+          href="/calculator"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
+          rel="noopener noreferrer"
+        >
+          <h2 className={`mb-3 text-2xl font-semibold`}>
+            Calculator{' '}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
         </a>
 
       </div>
