77a62b9b5a5a9f97783f5cb7a2a2cdc29c1fdb43
Agisna Revaldo <agisrifaldo75@gmail.com>
2023-11-19 23:06:01 +0700

    fix bug

--- src/app/calculator/page.js
+++ src/app/calculator/page.js
@@ -4,7 +4,7 @@ import './calculator.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faDeleteLeft } from '@fortawesome/free-solid-svg-icons'
 
-export default function page() {
+export default function Page() {
   let [result, setResult] = useState("0");
 
   const handleClick = (e) => {
