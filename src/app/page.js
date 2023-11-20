61f299f072ddf7e0c1aea4b4d2731ee92cfcd472
Agisna Revaldo <agisrifaldo75@gmail.com>
2023-11-20 14:31:31 +0700

    add page: chartlist

--- src/app/page.js
+++ src/app/page.js
@@ -78,6 +78,19 @@ export default function Home() {
           </h2>
         </a>
 
+        <a
+          href="/chartlist"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
+          rel="noopener noreferrer"
+        >
+          <h2 className={`mb-3 text-2xl font-semibold`}>
+            Calculator{' '}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+        </a>
+
       </div>
     </main>
   )
